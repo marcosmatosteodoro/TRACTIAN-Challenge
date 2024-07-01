f90c3d6729453f0db3e81fde68f3f859bb4631e7
Marcos Paulo <mp.matos25@gmail.com>
2024-07-01 04:04:19 -0300

    fix

--- src/theme.ts
+++ src/theme.ts
@@ -1,14 +1,6 @@
 import { extendTheme } from '@chakra-ui/react';
 
 const theme = extendTheme({
-  styles: {
-    global: {
-      body: {
-        bg: 'gray.50',
-        color: 'gray.800',
-      },
-    },
-  },
   colors: {
     brand: {
       50: '#e3f2f9',
